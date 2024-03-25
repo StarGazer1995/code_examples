99aaa504261014d01b0bcac0e738b2f516df0cc5
StarGazer1995 <gz19951230@outlook.com>
2024-03-25 19:34:35 +0800

    update code example of the first story: myth of override

---
+++ cpp_examples/01_myth_of_override/src/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<memory>
+
+class base {
+public:
+    base() = default;
+    void callPrivateFunction(){
+        privateFunction();
+    }
+private:
+    virtual void privateFunction(){
+        std::cout<<"The call comes from a private function"<<std::endl;
+    }
+};
+
+class derived : public base {
+public:
+    void privateFunction() override {
+        std::cout<<"The cal comes from the overrided function"<<std::endl;
+    }
+};
+
+int main(){
+    base *ptr = new derived();
+    ptr->callPrivateFunction();
+
+    derived *ptr_derived = new derived();
+    ptr_derived->privateFunction();
+    return 0;
+}
